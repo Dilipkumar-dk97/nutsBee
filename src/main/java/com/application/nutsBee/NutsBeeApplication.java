565d5fb2da1de54288f8b52e61d6cbe939947f4e
Dilip <dilikum603@gmail.com>
2024-06-13 13:47:13 +0530

    Fix Cors Error

--- src/main/java/com/application/nutsBee/NutsBeeApplication.java
+++ src/main/java/com/application/nutsBee/NutsBeeApplication.java
@@ -2,6 +2,9 @@ package com.application.nutsBee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NutsBeeApplication {
@@ -9,5 +12,18 @@ public class NutsBeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NutsBeeApplication.class, args);
 	}
-
+	
+	@Configuration
+    public class WebConfig implements WebMvcConfigurer {
+ 
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("Authorization", "Content-Type", "Access-Control-Allow-Origin")
+                    .allowCredentials(true)
+                    .maxAge(3600);
+        }
+    }
 }
