98eb7dd377c5c374be3f35cec86f9b4183fd649f
Dilip <dilikum603@gmail.com>
2024-06-15 15:56:59 +0530

    API implementation Address

--- src/main/java/com/application/nutsBee/Entity/Products.java
+++ src/main/java/com/application/nutsBee/Entity/Products.java
@@ -26,16 +26,6 @@ public class Products {
 
     @Column(name = "price_inc_tax")
     private double priceIncTax;
-    
-    @Column(name = "availability_status")
-    private double availabilityStatus;
-    
-    @Column(name = "product_desc")
-    private double productDesc;
-    
-    @Column(name = "product_category")
-    private double productCategory;
-   
 
     @Column(name = "price_exc_tax")
     private double priceExcTax;
